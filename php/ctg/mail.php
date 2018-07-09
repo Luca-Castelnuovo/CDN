81f0c436d1b49033ae6719a7e2429da1874fe1af
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-09 20:59:08 +0200

    test7ing mail function

--- php/ctg/mail.php
+++ php/ctg/mail.php
@@ -1,6 +1,8 @@
 <?php
 $headers = array('From: ' . $_POST['email'],'Content-Type:text/html','Reply-To: ' . $_POST['email']);
-$message = '<h1>Name from sender: ' . $_POST['name'] . '</h1><h6>IP from sender: ' . $_SERVER['REMOTE_ADDR'] . '</h6>'  . '<br /><p>' . nl2br($_POST['message']) . '</p>';
-mail('hujxu@slipry.net', $_POST['subject'], $message, implode("\r\n", $headers));
-header('Location: google.com');
-exit;
+$message = '<h1>Name from sender: ' . $_POST['name'] . '</h1><h6>IP from sender: ' . $_SERVER['REMOTE_ADDR'] . '</h6>'  . '<br /><p>' . $_POST['message'] . '</p>';
+
+if (mail('hujxu@slipry.net', $_POST['subject'], $message, implode("\r\n", $headers))) {
+    header('Location: google.com');
+    exit;
+}
