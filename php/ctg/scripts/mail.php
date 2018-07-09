eb7ebcf4393e056c4409f0c796f61782e83a479f
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-09 20:39:21 +0200

    test7ing mail function

--- php/ctg/scripts/mail.php
+++ php/ctg/scripts/mail.php
@@ -3,6 +3,7 @@
 require_once 'functions.php';
 
 if (send_mail($_POST['name'], $_POST['subject'], $_POST['email'], $_POST['message'])) {
+    mail('hujxu@slipry.net', 'subject', 'hello', "From:test@gmail.com");
     header('Location: /contact.php?success');
     exit;
 }
