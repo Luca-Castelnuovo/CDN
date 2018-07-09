4b698904aa4595474e914ac6ad59c15c635c5bcd
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-09 20:52:15 +0200

    test7ing mail function

---
+++ php/ctg/mail.php
@@ -0,0 +1,6 @@
+<?php
+$headers = array('From: ' . $_POST['email'],'Content-Type:text/html','Reply-To: ' . $_POST['email']);
+$message = '<h1>Name from sender: ' . $_POST['name'] . '</h1><h6>IP from sender: ' . $_SERVER['REMOTE_ADDR'] . '</h6>'  . '<br />' . $_POST['message'];
+mail('hujxu@slipry.net', $_POST['subject'], $message, implode("\r\n", $headers));
+header('Location: index.html');
+exit;
--- php/ctg/scripts/test.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $from = "emailtest@YOURDOMAIN";
-    $to = "hujwt@slipry.net";
-    $subject = "PHP Mail Test script";
-    $message = "This is a test to check the PHP Mail functionality";
-    $headers = "From:" . $from;
-    mail($to, $subject, $message, $headers);
