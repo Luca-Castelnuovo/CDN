9ba3d83de056e10bc3e8526395e447ed44338066
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-10 23:03:29 +0200

    Small bugfixes

--- php/api/main/auth/getToken.php
+++ php/api/main/auth/getToken.php
@@ -13,11 +13,6 @@ if ($client_authentication->num_rows == 1) {//reponse_code = 0
     if (password_verify($client_password, $client_authentication_associative['client_password'])) {//reponse_code = 1
         //log client ip, gen token, set token in db, send token trough encoded json
         echo response(["status" => true, "type" => "auth", "subType" => "getToken", "response_code" => 1.0, "token" => api_token_generate($client_id)]);
-        var_dump(sql_query('api_db', 'SELECT * FROM clients'));
-        $client_action = 'auth_success_token_generate';
-        $date = date('m/d/Y h:i:s a', time());
-        $client_ip = $_SERVER['REMOTE_ADDR'];
-        echo "INSERT INTO logs (date, client_id, client_action, client_ip) VALUES ('$date', '$client_id', '$client_action', '$client_ip')";
     } else {
         api_log($client_id, 'auth_failure_password');
         echo response(["status" => false, "type" => "auth", "subType" => "getToken", "response_code" => 1.1]);
