1e6cb31c8b2ba168360d45682b0b4f3713950cb7
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-10 18:30:26 +0200

    Small bugfixes

--- php/api/mail/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Load additional functions and configuration
-require '../authentication/functions.php';
-$config = parse_ini_file('../config/mail.ini');
-$response = clean_data($_POST['data']);
-echo response(["response" => $response]);
