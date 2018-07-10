b32a3d37c1e6e06142df186793ebe860dc33812d
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-10 13:53:49 +0200

    added phpmmailer

--- php/functions/mail.php
+++ php/functions/mail.php
@@ -1,4 +1,4 @@
-*<?php
+<?php
 $headers = array('From: ' . $_POST['email'],'Content-Type:text/html','Reply-To: ' . $_POST['email']);
 $message = '<h1>Name from sender: ' . $_POST['name'] . '</h1><h6>IP from sender: ' . $_SERVER['REMOTE_ADDR'] . '</h6>'  . '<br /><p>' . $_POST['message'] . '</p>';
 
