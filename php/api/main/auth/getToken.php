e434d57c6bea28b67df0c6426ae755cfcc326725
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-10 22:58:18 +0200

    Small bugfixes

--- php/api/main/auth/getToken.php
+++ php/api/main/auth/getToken.php
@@ -13,6 +13,7 @@ if ($client_authentication->num_rows == 1) {//reponse_code = 0
     if (password_verify($client_password, $client_authentication_associative['client_password'])) {//reponse_code = 1
         //log client ip, gen token, set token in db, send token trough encoded json
         echo response(["status" => true, "type" => "auth", "subType" => "getToken", "response_code" => 1.0, "token" => api_token_generate($client_id)]);
+        var_dump(sql_query('api_db', 'SELECT * FROM clients'));
     } else {
         api_log($client_id, 'auth_failure_password');
         echo response(["status" => false, "type" => "auth", "subType" => "getToken", "response_code" => 1.1]);
