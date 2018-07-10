c6f976b8d0f866ab76be89a83a0ab95825d3d659
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-10 20:44:03 +0200

    Small bugfixes

--- php/api/mail/ctg.php
+++ php/api/mail/ctg.php
@@ -3,7 +3,7 @@
 $name = $_POST['name'];
 $from = $_POST['email'];
 $subject = 'Close The Gap | ' . $_POST['subject'];
-$body = '<h1>This email is from: ' . $name  . '</h1>' . PHP_EOL . '<h2>Email: ' . $from  . '</h2>' . PHP_EOL . $_POST['body'];
+$body = '<h1>This email is from: ' . $name  . '</h1><br /><h2>Email: ' . $from  . '</h2>' . $_POST['body'];
 
 $data = ['to' => 'lucacastelnuovo@hetbaarnschlyceum.nl', 'subject' => $subject, 'body' => $body];
 
@@ -12,10 +12,10 @@ require '../main/functions.php';
 $result = api_call('POST', 'https://cdn.lucacastelnuovo.nl/php/api/mail/mail.php', $data);
 
 if ($result['status'] == 'true') {
-    echo '<h1>Mail succesvol verstuurd.</h1>' . PHP_EOL . '<h4>U wordt doorgestuurd in 3 seconden.</h4>';
+    echo '<h1>Mail succesvol verstuurd.</h1><br /><h4>U wordt doorgestuurd in 3 seconden.</h4>';
 //header('Refresh: 3; URL=https://ctg.bss.design/');
 } else {
-    echo '<h1>Mail niet verstuurd.</h1>' . PHP_EOL . '<h2>Probeer het later opnieuw AUB.</h2>' . PHP_EOL . '<h4>U wordt doorgestuurd in 5 seconden.</h4>';
+    echo '<h1>Mail niet verstuurd.</h1><br /><h2>Probeer het later opnieuw AUB.</h2><br /><h4>U wordt doorgestuurd in 5 seconden.</h4>';
     //header('Refresh: 5; URL=https://ctg.bss.design/');
     echo $result['response_code'];
 }
