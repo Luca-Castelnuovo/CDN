56d6e8dbebff0c89559fc3aabdaa53d89ecdd8b8
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-10 13:47:48 +0200

    Small bugfixes

--- php/ctg/mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-$headers = array('From: ' . $_POST['email'],'Content-Type:text/html','Reply-To: ' . $_POST['email']);
-$message = '<h1>Name from sender: ' . $_POST['name'] . '</h1><h6>IP from sender: ' . $_SERVER['REMOTE_ADDR'] . '</h6>'  . '<br /><p>' . $_POST['message'] . '</p>';
-
-if (mail('ltcastelnuovo@yandex.com', $_POST['subject'], $message, implode("\r\n", $headers))) {
-    header('Location: index.html');
-    exit;
-}
