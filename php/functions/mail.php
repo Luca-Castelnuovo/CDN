ccb3e34f0a7c9b5f4d93f136b4eee81310283979
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-10 14:36:49 +0200

    Small bugfixes

--- php/functions/mail.php
+++ php/functions/mail.php
@@ -16,7 +16,7 @@ try {
     $mail->SMTPAuth = true;                               // Enable SMTP authentication
     $mail->Username = 'no-reply@lucacastelnuovo.nl';      // SMTP username
     $mail->Password = '$87N12OHC3Tctrk7f%t%';             // SMTP password
-    $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+    $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 465;                                    // TCP port to connect to
 
     //Recipients
