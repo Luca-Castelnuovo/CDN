dfdf628211a909ed894176ece69f2103a637af2e
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-10 18:49:08 +0200

    Small bugfixes

---
+++ php/api/mail/bss ctg.php
@@ -0,0 +1,23 @@
+<?php
+
+$message = 'test';
+
+$url = 'https://cdn.lucacastelnuovo.nl/php/api/mail/mail.php';
+$data = array('to' => 'lucacastelnuovo@hetbaarnschlyceum.nl', 'subject' => $_POST['subject'], 'message' => $message);
+
+$options = array(
+    'http' => array(
+        'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method'  => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+
+$context  = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
+
+if ($result === false) {
+    echo 'error';
+}
+
+var_dump($result);
