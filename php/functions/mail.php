0e9e0da396f9031699277370989f7000c7848f96
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-10 14:18:33 +0200

    Small bugfixes

--- php/functions/mail.php
+++ php/functions/mail.php
@@ -1,8 +1,10 @@
 <?php
-$headers = array('From: ' . $_POST['email'],'Content-Type:text/html','Reply-To: ' . $_POST['email']);
-$message = '<h1>Name from sender: ' . $_POST['name'] . '</h1><h6>IP from sender: ' . $_SERVER['REMOTE_ADDR'] . '</h6>'  . '<br /><p>' . $_POST['message'] . '</p>';
-
-if (mail('ltcastelnuovo@yandex.com', $_POST['subject'], $message, implode("\r\n", $headers))) {
-    header('Location: index.html');
-    exit;
-}
+// $headers = array('From: ' . $_POST['email'],'Content-Type:text/html','Reply-To: ' . $_POST['email']);
+// $message = '<h1>Name from sender: ' . $_POST['name'] . '</h1><h6>IP from sender: ' . $_SERVER['REMOTE_ADDR'] . '</h6>'  . '<br /><p>' . $_POST['message'] . '</p>';
+//
+// if (mail('ltcastelnuovo@yandex.com', $_POST['subject'], $message, implode("\r\n", $headers))) {
+//     header('Location: index.html');
+//     exit;
+// }
+require 'PHPMailerAutoload.php';
+$mail = new PHPMailer;
