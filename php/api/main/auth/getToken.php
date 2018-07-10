261887dce3b1a6595871074160b9bb1c9b721cc4
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-10 22:51:30 +0200

    Small bugfixes

--- php/api/main/auth/getToken.php
+++ php/api/main/auth/getToken.php
@@ -10,13 +10,13 @@ $client_authentication = sql_query('api_db', $query, false);
 
 if ($client_authentication->num_rows == 1) {//reponse_code = 0
     $client_authentication_associative = $client_authentication->fetch_assoc();
-    if (password_verify($client_authentication_associative['client_password'], $client_password)) {//reponse_code = 1
+    if (password_verify($client_password, $client_authentication_associative['client_password'])) {//reponse_code = 1
         //log client ip, gen token, set token in db, send token trough encoded json
         echo response(["status" => true, "type" => "auth", "subType" => "getToken", "response_code" => 1.0, "token" => api_token_generate($client_id)]);
     } else {
         api_log($client_id, 'auth_failure_password');
         echo response(["status" => false, "type" => "auth", "subType" => "getToken", "response_code" => 1.1]);
-        echo $client_password;
+        echo $client_password . '<br />' . $client_authentication_associative['client_password'];
     }
 } else {
     api_log('unknown', 'auth_failure_unknown_user');
