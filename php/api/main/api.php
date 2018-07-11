748c8d362dfa05a9a32545dd892ac6bb0f8bf474
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-11 12:21:52 +0200

    Small bugfixes

--- php/api/main/api.php
+++ php/api/main/api.php
@@ -42,7 +42,7 @@ function api_token_generate($client_id)
     $query = "INSERT INTO tokens (client_id,client_token) VALUES ('{$client_token}','{$client_id}')";
     sql_query('api_db', $query, false);
     api_log($client_id, 'auth_success_api_token_generate');
-    return $client_token;
+    return $query;
 }
 
 
