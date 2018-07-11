cea0de7e125a179baead24f43b1fe34c06e6df0c
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-11 20:21:20 +0200

    Small bugfixes

--- php/api/main/includes/overig.php
+++ php/api/main/includes/overig.php
@@ -66,10 +66,11 @@ function validate_client_id_and_pass($client_id, $client_password)
 }
 
 //Return false on empty var
-function is_empty($data, $error)
+function is_empty_var($data, $error)
 {
     if (!isset($data)) {
         echo $error;
         exit();
     }
+    exit();
 }
