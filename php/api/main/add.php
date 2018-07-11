563357f24da5b8f6f5ed6b60865da5557a702220
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-11 20:07:55 +0200

    Small bugfixes

--- php/api/main/add.php
+++ php/api/main/add.php
@@ -12,12 +12,14 @@ $query_result = sql_query('api_db', $query, false);
 
 //Check is user exists
 if ($query_result->num_rows != 1) {//reponse_code = 0
+    echo response(["status" => false, "response_code" => 0]);
     exit;
 }
 
 //Check password
 $query_result_assoc = $query_result->fetch_assoc();
 if (!password_verify($client_password, $query_result_assoc['client_password'])) {
+    echo response(["status" => false, "response_code" => 1]);
     exit;
 }
 
