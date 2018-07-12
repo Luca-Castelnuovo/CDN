c6b2e38e5f3329eb3a3e9fb9409d3ae21cc5a012
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-12 10:00:56 +0200

    Small bugfixes

--- php/api/main/add/index.php
+++ php/api/main/add/index.php
@@ -30,12 +30,13 @@
                 <input type="text" name="client_ip" class="text validate" id="client_ip" autocomplete="off">
             </div>
             <button id="submit"><i class="spinner"></i> <span class="state">Generate Token</span></button>
+        </form>
         <?php
 } else {
         require $_SERVER['DOCUMENT_ROOT'] . '/php/api/main/init.php';
-        echo '<p class="title">Client Token:</p><textarea rows="9" readonly>' . clean_data($_GET['client_token']) . '</textarea><button onclick="window.location = \'/php/api/main/add/index.php?client_token=0\';"><span class="state">Back</span></button>';
+        echo '<form action="index.php" class="login">
+        <p class="title">Client Token:</p><textarea rows="9" readonly>' . clean_data($_GET['client_token']) . '</textarea><button type="submit"><span class="state">Back</span></button></form>';
     } ?>
-        </form>
     </div>
     <script src="/js/vanilla/randomBackground.js"></script><script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
     </script><script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script><script src= 'ajax.js'></script></body>
