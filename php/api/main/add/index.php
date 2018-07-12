32bed8ef1a084a4dc0de7a9253a8982ddcc1a962
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-12 14:41:34 +0200

    Small bugfixes

--- php/api/main/add/index.php
+++ php/api/main/add/index.php
@@ -39,5 +39,5 @@
     } ?>
     </div>
     <script src="/js/vanilla/randomBackground.js"></script><script src="//ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
-    </script><script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script><script src= 'ajax.js'></script></body>
+    </script><script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script><script src= 'main.js'></script></body>
 </html>
