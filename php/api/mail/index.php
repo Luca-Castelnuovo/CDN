49cea786b4cd75bef65d5062c1fa80789a46aa31
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-12 15:14:35 +0200

    Small bugfixes

--- php/api/mail/index.php
+++ php/api/mail/index.php
@@ -41,15 +41,15 @@ $mail->addAddress(clean_data($_POST['to']));
 
 //Content
 if (!isset($_POST['to'])) {//reponse_code = 0
-    echo response(["status" => false, "type" => "mail", "response_code" => 0]);
+    echo response(["status" => false, "response_code" => 0]);
     exit;
 }
 if (!isset($_POST['subject'])) {//reponse_code = 0
-    echo response(["status" => false, "type" => "mail", "response_code" => 0.1]);
+    echo response(["status" => false, "response_code" => 0.1]);
     exit;
 }
 if (!isset($_POST['body'])) {//reponse_code = 0
-    echo response(["status" => false, "type" => "mail", "response_code" => 0.2]);
+    echo response(["status" => false, "response_code" => 0.2]);
     exit;
 }
 
