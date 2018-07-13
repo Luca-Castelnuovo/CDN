6e454703e5ab9b2154f38db9f60786179334960b
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-13 14:02:49 +0200

    removed api folder

--- api/main/init.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require $_SERVER['DOCUMENT_ROOT'] . '/php/api/main/includes/sql.php';
-require $_SERVER['DOCUMENT_ROOT'] . '/php/api/main/includes/api.php';
-require $_SERVER['DOCUMENT_ROOT'] . '/php/api/main/includes/overig.php';
