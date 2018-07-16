3a8378b6618221736f85701ed80bc59aa6887e8a
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-16 14:39:19 +0200

    Small bugfixes

--- js/vanilla/background.php
+++ js/vanilla/background.php
@@ -1,4 +1,4 @@
 <?php
 
 $background = isset($_GET['background']) ? $_GET['background'] : 0;
-echo "document.body.style.backgroundImage = \"url('https://cdn.lucacastelnuovo.nl/images/backgrounds/\" + $background + \".jpg')\";"
+echo "document.body.style.backgroundImage = \"url('https://cdn.lucacastelnuovo.nl/images/backgrounds/\" + $background + \".jpg')\";";
