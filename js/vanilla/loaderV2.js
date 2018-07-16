3a4c5834c12300615c927977a318828c1bee7ada
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-07-16 14:32:25 +0200

    Small bugfixes

---
+++ js/vanilla/loaderV2.js
@@ -0,0 +1,23 @@
+//Define content and loader
+var $content = $('.content');
+var $loader = $('.loader');
+
+//Hide content and show loader
+$content.addClass('hidden');
+$loader.addClass('loading');
+
+//Show content and hide loader
+function stopLoading() {
+    $content.fadeOut(0,function(){
+        $loader.removeClass('loading');
+        $content.removeClass('hidden').fadeIn('fast');
+    });
+}
+
+//Hide content and show loader
+function startLoading() {
+    $content.fadeOut('fast', function() {
+      $content.addClass('hidden').fadeIn(0);
+      $loader.addClass('loading');
+    });
+}
