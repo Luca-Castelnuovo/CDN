0b0f8b9571a5dfb73c5a21da12300fac3b64f9b8
Luca Castelnuovo <Luca-Castelnuovo>
2018-07-19 10:38:23 +0200

    removed old unused scripts

--- js/vanilla/background.php
+++ js/vanilla/background.php
@@ -1,11 +1,15 @@
 <?php
 
-$background = isset($_GET['background']) ? $_GET['background'] : 'null';
+$background = !empty($_GET['background']) ? $_GET['background'] : 'null';
 
 header('Content-Type: application/javascript');
 
-if ($background == 'null') {
-    echo "document.body.style.backgroundImage = \"url('https://cdn.lucacastelnuovo.nl/images/backgrounds/\" + Math.floor(10 * Math.random()) + \".jpg')\";";
+if (empty($_GET['url'])) {
+    if ($background == 'null') {
+        echo "document.body.style.backgroundImage = \"url('https://cdn.lucacastelnuovo.nl/images/backgrounds/\" + Math.floor(10 * Math.random()) + \".jpg')\";";
+    } else {
+        echo "document.body.style.backgroundImage = \"url('https://cdn.lucacastelnuovo.nl/images/backgrounds/\" + $background + \".jpg')\";";
+    }
 } else {
-    echo "document.body.style.backgroundImage = \"url('https://cdn.lucacastelnuovo.nl/images/backgrounds/\" + $background + \".jpg')\";";
+    echo "document.body.style.backgroundImage = \"url({$_GET['url']})\";";
 }
