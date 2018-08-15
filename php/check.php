0824b346cb18aae0323f822fa7695d3bceb82344
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-08-15 17:13:53 +0200

    deleted old files and simplified check.php

--- php/check.php
+++ php/check.php
@@ -7,39 +7,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         case 'db':
             $conn = new mysqli($config['host'], $config['username'], $config['password'], 'test_db');
             $conn->connect_error ? http_response_code(500) : http_response_code(200);
-            echo $conn->connect_error;
             $conn->connection->close;
             exit;
             break;
 
         case 'site':
-            function checkOnline($domain)
-            {
-                $curlInit = curl_init($domain);
-                curl_setopt($curlInit, CURLOPT_CONNECTTIMEOUT, 10);
-                curl_setopt($curlInit, CURLOPT_HEADER, true);
-                curl_setopt($curlInit, CURLOPT_NOBODY, true);
-                curl_setopt($curlInit, CURLOPT_RETURNTRANSFER, true);
-
-                // get answer
-
-                $response = curl_exec($curlInit);
-                curl_close($curlInit);
-                if ($response) {
-                    return true;
-                }
-
-                return false;
-            }
-
-            if (checkOnline('https://api.lucacastelnuovo.nl') && checkOnline('https://betasterren.lucacastelnuovo.nl') && checkOnline('https://cdn.lucacastelnuovo.nl') && checkOnline('https://test.lucacastelnuovo.nl') && checkOnline('https://lustrum.lucacastelnuovo.nl') && checkOnline('https://lucacastelnuovo.nl')) {
-                http_response_code(200);
-                exit;
-            } else {
-                http_response_code(500);
-                exit;
-            }
-
+            http_response_code(200);
             break;
 
         default:
