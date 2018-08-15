d00649cbc73f48d286234b3022382f5c3e6b66fc
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-08-15 16:22:11 +0200

    Made some changes [deploy:Production]

---
+++ js/loader.js
@@ -0,0 +1,23 @@
+//Define content and loader
+const content = document.querySelector('.content');
+const loader = document.querySelector('.loader');
+
+//Hide content and show loader
+content.addClass('hidden');
+loader.addClass('loading');
+
+//Show content and hide loader
+function stopLoading() {
+    content.fadeOut(0,function(){
+        loader.removeClass('loading');
+        content.removeClass('hidden').fadeIn('fast');
+    });
+}
+
+//Hide content and show loader
+function startLoading() {
+    content.fadeOut('fast', function() {
+      content.addClass('hidden').fadeIn(0);
+      loader.addClass('loading');
+    });
+}
--- php/checkDB.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$config = parse_ini_file('/var/www/test.lucacastelnuovo.nl/config.ini');
-
-$conn = new mysqli($config['host'], $config['username'], $config['password'], 'test_db');
-
-$conn->connect_error ? http_response_code(500) : http_response_code(200);exit;
