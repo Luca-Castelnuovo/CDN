efdc19edfac26e905d08244b3b1dd34167f5088f
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-08-25 10:22:05 +0200

    added particles.js

---
+++ build/js/betasterren/particles.js
@@ -0,0 +1,1 @@
+"use strict";var particles=Particles.init({selector:".background",maxParticles:500,color:["#003d14","#f5a82a","#7a0036"],sizeVariations:5,responsive:[{breakpoint:768,options:{maxParticles:400}},{breakpoint:425,options:{maxParticles:200}},{breakpoint:320,options:{maxParticles:100}}]});
