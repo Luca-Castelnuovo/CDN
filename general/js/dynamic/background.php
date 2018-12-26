59127f1f48134348637ac3164670329aeebbb6d9
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-26 11:58:58 +0100

    update

--- general/js/dynamic/background.php
+++ general/js/dynamic/background.php
@@ -3,9 +3,9 @@
 ?>
 
 <?php
-    if (empty($_GET['background'])) {
+    if (empty($_GET['bg'])) {
         echo 'document.body.style.backgroundImage = "url(\'https://cdn.lucacastelnuovo.nl/general/images/backgrounds/" + Math.floor(10 * Math.random()) + ".jpg\')";';
     } else {
-        echo "document.body.style.backgroundImage = \"url('https://cdn.lucacastelnuovo.nl/general/images/backgrounds/{$_GET['background']}.jpg')\";";
+        echo "document.body.style.backgroundImage = \"url('https://cdn.lucacastelnuovo.nl/general/images/backgrounds/{$_GET['bg']}.jpg')\";";
     }
 ?>
