93a52008f4ee3712fce7fca3ae677d1f5854bae5
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-28 19:02:51 +0100

    update

--- panel/includes/init.php
+++ panel/includes/init.php
@@ -2,15 +2,15 @@
 
 session_start();
 
-$GLOBALS['config'] = require $_SERVER['DOCUMENT_ROOT'] . '/includes/config.php';
+$GLOBALS['config'] = require $_SERVER['DOCUMENT_ROOT'] . '/panel/includes/config.php';
 
-require $_SERVER['DOCUMENT_ROOT'] . '/includes/auth_sec/auth.php';
-require $_SERVER['DOCUMENT_ROOT'] . '/includes/auth_sec/authentication.php';
-require $_SERVER['DOCUMENT_ROOT'] . '/includes/auth_sec/security.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/panel/includes/auth_sec/auth.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/panel/includes/auth_sec/authentication.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/panel/includes/auth_sec/security.php';
 
-require $_SERVER['DOCUMENT_ROOT'] . '/includes/functions/files.php';
-require $_SERVER['DOCUMENT_ROOT'] . '/includes/functions/folders.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/panel/includes/functions/files.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/panel/includes/functions/folders.php';
 
-require $_SERVER['DOCUMENT_ROOT'] . '/includes/io/output.php';
-require $_SERVER['DOCUMENT_ROOT'] . '/includes/io/sql.php';
-require $_SERVER['DOCUMENT_ROOT'] . '/includes/io/template.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/panel/includes/io/output.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/panel/includes/io/sql.php';
+require $_SERVER['DOCUMENT_ROOT'] . '/panel/includes/io/template.php';
