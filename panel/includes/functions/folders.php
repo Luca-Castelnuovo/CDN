95deb8ed43c75d6efcd15ecb796032f342dcbe20
Luca-Castelnuovo <ltcastelnuovo@gmail.com>
2018-12-28 20:09:09 +0100

    update

--- panel/includes/functions/folders.php
+++ panel/includes/functions/folders.php
@@ -1,23 +1,58 @@
 <?php
 
 function folders_list() {
-    $projects = sql_select('folders', 'id,name', "true", false);
+    $folders = sql_select('folders', 'id,name', "is_subfolder='0'", false);
 
     echo '<style>.blue-icon{color:#2962ff}</style>';
     echo '<ul class="collection with-header">';
     echo '<li class="collection-header"><h4>Projects</h4></li>';
 
-    if ($projects->num_rows != 0) {
+    if ($folders->num_rows != 0) {
         $CSRFtoken = csrf_gen();
 
-        while ($project = $projects->fetch_assoc()) {
+        while ($folder = $folders->fetch_assoc()) {
             echo <<<HTML
             <li class="collection-item">
                 <div>
-                    <a href="?project_id={$project['id']}">
-                        {$project['name']}
+                    <a href="?folder_id={$folder['id']}">
+                        {$folder['name']}
                     </a>
-                    <a href="?project_id={$project['id']}&CSRFtoken={$CSRFtoken}" class="secondary-content" onclick="return confirm('Are you sure?')">
+                    <a href="/folders/delete?folder_id={$folder['id']}&CSRFtoken={$CSRFtoken}" class="secondary-content" onclick="return confirm('Are you sure?')">
+                        <i class="material-icons blue-icon">delete</i>
+                    </a>
+                </div>
+            </li>
+HTML;
+        }
+    } else {
+        echo '<li class="collection-item">You don\'t have any projects.</li>';
+    }
+
+    echo '</ul>';
+    echo '<a href="/project" class="btn waves-effect blue accent-4">Create Project</a>';
+}
+
+
+function sub_folders_list($folder_id) {
+    $folder_id = check_data($folder_id, true, 'Folder ID', true, '/panel/home');
+
+    $folders = sql_select('folders', 'name', "parent_id='{$folder_id}' AND is_subfolder='1'", true);
+
+    echo '<style>.blue-icon{color:#2962ff}</style>';
+    echo '<ul class="collection with-header">';
+    echo '<li class="collection-header"><h4>Projects</h4></li>';
+
+    if ($folders->num_rows != 0) {
+        $CSRFtoken = csrf_gen();
+
+        while ($folder = $folders->fetch_assoc()) {
+            echo <<<HTML
+            <li class="collection-item">
+                <div>
+                    <a href="?folder_id={$folder['id']}">
+                        {$folder['name']}
+                    </a>
+                    <a href="/folders/delete?folder_id={$folder['id']}&CSRFtoken={$CSRFtoken}" class="secondary-content" onclick="return confirm('Are you sure?')">
                         <i class="material-icons blue-icon">delete</i>
                     </a>
                 </div>
@@ -37,16 +72,20 @@ function folders_info($folder_id)
 {
     $folder_id = check_data($folder_id, true, 'Folder ID', true, '/panel/home');
 
-    $project = sql_select('folders', 'name', "id='{$project_id}'", true);
+    $folder = sql_select('folders', 'is_subfolder,name', "id='{$folder_id}'", true);
 
-    if (empty($project)) {
-        redirect('/panel/home', 'Project doesn\'t exist');
+    if (empty($folder)) {
+        redirect('/panel/home', 'Folder doesn\'t exist');
     }
 
     echo <<<HTML
     <div class="row">
 HTML;
-    files_list($folder_id);
+    if ($folder['is_subfolder']) {
+        sub_folders_list($folder_id);
+    } else {
+        files_list($folder_id);
+    }
     echo '</div>';
 }
 
