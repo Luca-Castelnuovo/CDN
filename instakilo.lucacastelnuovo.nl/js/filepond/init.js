7f30abd02d1962484127627843888d3e94ebbba8
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-01-14 22:10:32 +0100

    update

---
+++ instakilo.lucacastelnuovo.nl/js/filepond/init.js
@@ -0,0 +1,20 @@
+FilePond.registerPlugin(
+    FilePondPluginFileValidateSize,
+    FilePondPluginFileValidateType,
+    FilePondPluginImageExifOrientation,
+    FilePondPluginImageCrop,
+    FilePondPluginImageTransform
+);
+
+FilePond.setOptions({
+    maxFileSize: '2MB',
+    acceptedFileTypes: [
+        'image/png',
+        'image/jpg',
+        'image/jpeg'
+    ],
+    imageCropAspectRatio: '1:1',
+    server: '/includes/upload.php'
+});
+
+var pond = FilePond.create(document.querySelector('input[type="file"]'));
