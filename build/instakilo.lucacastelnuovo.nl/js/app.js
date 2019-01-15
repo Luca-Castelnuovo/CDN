1772c80b4ac2731633e02e158236dbd1629ecc15
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-01-15 16:08:30 +0100

    updatye

--- build/instakilo.lucacastelnuovo.nl/js/app.js
+++ build/instakilo.lucacastelnuovo.nl/js/app.js
@@ -248,7 +248,7 @@ function feed_render_post(post) {
                 <div class="card-image"><img id="post_image" class="materialboxed" data-caption="${post.caption}" src="${post.img_url}"></div>
                 <div class="card-content">
                     <p>
-                        <span id="post_owner"><a href="/u/${post.username}">${post.username}</a></span> ${post.caption}
+                        <span id="post_owner"><a href="/u/${post.username}" class="tt-none">${post.username}</a></span> ${post.caption}
                         ${post.user_is_owner ? `<a href="/posts/edit/${post.id}" class="secondary-content"><i class="material-icons blue-icon">edit</i></a>` : ''}
                     </p>
                 </div>
