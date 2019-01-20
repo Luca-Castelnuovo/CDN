c6df4da0123036c384b456e8b7c62b687be34273
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-01-20 17:16:20 +0100

    update

--- build/instakilo.lucacastelnuovo.nl/js/app.js
+++ build/instakilo.lucacastelnuovo.nl/js/app.js
@@ -473,41 +473,44 @@ function user_followers(username) {
 
 function user_following(username) {
     GETrequest(`/u/${username}/following`, function(response) {
-        var followings_html = [];
+        var users_owner_is_following = [];
         var following_html;
 
-        for (following of response.following) {
-            following_html = `
+        for (user_owner_is_following of response.following) {
+            users_owner_is_following.push(`
                 <li class="collection-item avatar">
                     <div class="row mb-0">
-                        <a href="/u/${following.username}" class="blue-text">
-                            <img src="${following.profile_picture}" onerror="this.src='https://github.com/identicons/${following.username}.png'" class="circle" />
-                            <span class="title">${following.username}</span>
+                        <a href="/u/${user_owner_is_following.username}" class="blue-text">
+                            <img
+                                src="${user_owner_is_following.profile_picture}"
+                                onerror="this.src='https://github.com/identicons/${user_owner_is_following.username}.png'"
+                                class="circle"
+                            />
+                            <span class="title">${user_owner_is_following.username}</span>
                         </a>
-                        <a onclick="${ following.is_following ? `user_follow('${following.username}')` : `user_undo_follow('${following.username}')` }" class="waves-effect btn right ${ following.is_following ? `grey lighten-5 black-text tooltipped" data-position="right" data-tooltip="Unfollow` : 'waves-light blue accent-4 "'}">
-                            ${following.is_following ? "Following" : "Follow"}
+                        <a onclick="${user_owner_is_following.is_following ? `user_follow('${user_owner_is_following.username}')` : `user_undo_follow('${user_owner_is_following.username}')` }" class="waves-effect btn right ${user_owner_is_following.is_following ? `grey lighten-5 black-text tooltipped" data-position="right" data-tooltip="Unfollow` : 'waves-light blue accent-4 "'}">
+                            ${user_owner_is_following.is_following ? "Following" : "Follow"}
                         </a>
                     </div>
                 </li>
-            `;
-            followings_html.push(following_html);
+            `);
         }
 
-        document.querySelector("#followers_container")
-            .innerHTML = followings_html.join("");
+        document.querySelector("#following_container")
+            .innerHTML = users_owner_is_following.join("");
 
         var tooltips = M.Tooltip.init(document.querySelectorAll(".tooltipped"), {});
-        var modal = M.Modal.getInstance(document.querySelector("#followers_modal"));
+        var modal = M.Modal.getInstance(document.querySelector("#following_modal"));
         modal.open();
     });
 }
 
 function user_follow(user_name) {
-    console.log(user_name);
+    console.log('follow', user_name);
 }
 
 function user_undo_follow(user_name) {
-    console.log(user_name);
+    console.log('undo_follow', user_name);
 }
 
 
