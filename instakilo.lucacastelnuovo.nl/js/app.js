cdf9b701628216e60c37923b9059108896abd9d7
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-01-22 19:17:38 +0100

    upd

--- instakilo.lucacastelnuovo.nl/js/app.js
+++ instakilo.lucacastelnuovo.nl/js/app.js
@@ -1,8 +1,8 @@
-// @codekit-prepend "lazyload.js";
-
 // @codekit-prepend "init.js";
 // @codekit-prepend "request.js";
 // @codekit-prepend "feed_posts.js";
 // @codekit-prepend "feed_messages.js";
 // @codekit-prepend "user.js";
 // @codekit-prepend "hash_tag.js";
+
+// @codekit-prepend "lazyload.js";
