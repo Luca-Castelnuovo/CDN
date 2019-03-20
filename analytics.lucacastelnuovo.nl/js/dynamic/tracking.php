138c73660bae8e045b4ec12ec033d2edce730254
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-03-20 19:10:35 +0100

    update

--- analytics.lucacastelnuovo.nl/js/dynamic/tracking.php
+++ analytics.lucacastelnuovo.nl/js/dynamic/tracking.php
@@ -12,35 +12,27 @@ if (!isset($_GET['site_id']) || empty($_GET['site_id'])) {
 
 if (isset($_GET['track_subdomains']) && !empty($_GET['track_subdomains'])) {
     $track_subdomains = "_paq.push(['setDomains', '*.{$_GET['track_subdomains']}']);";
-    $exclude_outlinks = "_paq(['setDomains', ['*.{$_GET['track_subdomains']}']]);";
 }
 
 echo <<<JS
-    var _paq = window._paq || [];
-
-    {$track_subdomains}
-    {$exclude_outlinks}
-
-    _paq.push(['setDownloadClasses', "track-download"]); // Download links have to have an 'track-download' class
-    _paq.push(['setLinkClasses', "track-external"]); // External links have to have an 'track-external' class
-    _paq.push(['setIgnoreClasses', "track-disabled"]); // Links with this class wont be tracked (e.g. testing)
-
-    _paq.push(["trackPageView"]);
-
-    _paq.push(["enableLinkTracking"]);
-    _paq.push(['enableHeartBeatTimer']);
-
-    (function() {
-      var u = "https://analytics.lucacastelnuovo.nl/";
-      _paq.push(["setTrackerUrl", u + "matomo.php"]);
-      _paq.push(["setSiteId", {$_GET['site_id']}]);
-      var d = document,
-        g = d.createElement("script"),
-        s = d.getElementsByTagName("script")[0];
-      g.type = "text/javascript";
-      g.async = true;
-      g.defer = true;
-      g.src = u + "matomo.js";
-      s.parentNode.insertBefore(g, s);
-    })();
+var _paq = window._paq || [];
+{$track_subdomains}
+_paq.push(['setDownloadClasses', "track-download"]);
+_paq.push(['setLinkClasses', "track-external"]);
+_paq.push(['setIgnoreClasses', "track-disabled"]);
+_paq.push(["trackPageView"]);
+_paq.push(["enableLinkTracking"]);
+(function() {
+  var u = "https://analytics.lucacastelnuovo.nl/";
+  _paq.push(["setTrackerUrl", u + "matomo.php"]);
+  _paq.push(["setSiteId", {$_GET['site_id']}]);
+  var d = document,
+    g = d.createElement("script"),
+    s = d.getElementsByTagName("script")[0];
+  g.type = "text/javascript";
+  g.async = true;
+  g.defer = true;
+  g.src = u + "matomo.js";
+  s.parentNode.insertBefore(g, s);
+})();
 JS;
