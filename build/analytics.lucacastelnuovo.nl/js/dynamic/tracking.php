cf4b466dc8b2cb13e2fbb454c658966221a920fd
Luca Castelnuovo <ltcastelnuovo@gmail.com>
2019-03-20 16:33:09 +0100

    update

--- build/analytics.lucacastelnuovo.nl/js/dynamic/tracking.php
+++ build/analytics.lucacastelnuovo.nl/js/dynamic/tracking.php
@@ -2,6 +2,9 @@
 
 header('Content-Type: application/javascript');
 
+$track_subdomains;
+$exclude_outlinks;
+
 if (!isset($_GET['site_id']) || empty($_GET['site_id'])) {
     echo 'alert("please specify an site_id")';
     exit;
@@ -9,15 +12,23 @@ if (!isset($_GET['site_id']) || empty($_GET['site_id'])) {
 
 if (isset($_GET['track_subdomains']) && !empty($_GET['track_subdomains'])) {
     $track_subdomains = "_paq.push(['setDomains', '*.{$_GET['track_subdomains']}']);";
+    $exclude_outlinks = "_paq(['setDomains', ['*.{$_GET['track_subdomains']}']]);";
 }
 
-echo <<<JS
+echo preg_replace("/\r|\n/", '', <<<JS
     var _paq = window._paq || [];
 
     {$track_subdomains}
+    {$exclude_outlinks}
+
+    _paq.push(['setDownloadClasses', "track-download"]); // Download links have to have an 'track-download' class
+    _paq.push(['setLinkClasses', "track-external"]); // External links have to have an 'track-external' class
+    _paq.push(['setIgnoreClasses', "track-disabled"]); // Links with this class wont be tracked (e.g. testing)
 
     _paq.push(["trackPageView"]);
+
     _paq.push(["enableLinkTracking"]);
+    _paq.push(['enableHeartBeatTimer']);
 
     (function() {
       var u = "https://analytics.lucacastelnuovo.nl/";
@@ -32,4 +43,4 @@ echo <<<JS
       g.src = u + "matomo.js";
       s.parentNode.insertBefore(g, s);
     })();
-JS;
+JS);
